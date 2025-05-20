807b58bc17e4cf4fa6780a3dc1dcd84c3b632c35
uch4 <joaquin.uchagallo@gmail.com>
2025-05-20 19:15:38 -0300

    fix: Correciones finales de compilacion, Plugin.Firebase y compatibilidad con .NET 9

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,4 +1,10 @@
-﻿using Microsoft.Extensions.Logging;
+﻿// MauiProgram.cs
+using Microsoft.Extensions.Logging;
+using Plugin.Firebase.Auth;        // Necesario para AddFirebaseAuth()
+using Plugin.Firebase.Firestore;    // Necesario para AddFirestore()
+//using Plugin.Firebase.Shared;       // Necesario para ConfigureFirebase()
+
+using _1mas.Services; // Asegúrate de que este namespace apunte a donde están tus servicios
 
 namespace _1mas
 {
@@ -13,11 +19,22 @@ namespace _1mas
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                })
+                // AÑADE ESTO: Inicialización de Plugin.Firebase y registro de servicios de Firebase
+                .ConfigureFirebase(firebaseBuilder =>
+                {
+                    firebaseBuilder.AddFirebaseAuth();      // Habilita Firebase Authentication
+                    firebaseBuilder.AddFirestore();         // Habilita Firebase Firestore
+                    // Puedes añadir otros servicios si los necesitas (ej. AddCloudMessaging(), AddAnalytics())
                 });
 
-#if DEBUG
-    		builder.Logging.AddDebug();
-#endif
+            // REGISTRA TUS SERVICIOS PERSONALIZADOS
+            builder.Services.AddSingleton<FirebaseAuthService>();
+            builder.Services.AddSingleton<FirestoreService>();
+
+            #if DEBUG
+                builder.Logging.AddDebug();
+            #endif
 
             return builder.Build();
         }
